5f17b268f8b10da5632ef638c734379b82a1a233
Crois0509 <crois0509@naver.com>
2024-08-08 17:24:49 +0900

    feat: AuditionManager/cast 메서드 추가

--- CodeStarterCamp_Week5/AuditionManager.swift
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -3,4 +3,35 @@ import Foundation
 struct AuditionManager {
     let totalApplicantsList: [Person]
     var passedApplicantsList: [Person] = []
+    
+    mutating func cast(to talent: Person) throws {
+        if talent is Talent {
+            guard let talents: Talent = talent as? Talent else {
+                print("안타깝지만 불합격입니다.")
+                throw TalentTypeError.notTalent
+            }
+            
+            guard talents is BadPersonality else {
+                print("안타깝지만 불합격입니다.")
+                throw TalentTypeError.notTalent
+            }
+            
+            guard talents.acting != Level.A else {
+                print("축하드립니다! 합격입니다.")
+                return passedApplicantsList.append(talent)
+            }
+            
+            guard talents.dancing != Level.A else {
+                print("축하드립니다! 합격입니다.")
+                return passedApplicantsList.append(talent)
+            }
+            
+            guard talents.singing != Level.A else {
+                print("축하드립니다! 합격입니다.")
+                return passedApplicantsList.append(talent)
+            }
+            
+            print("안타깝지만 불합격입니다.")
+        }
+    }
 }
