f461ecad7c20e1c45b0a1c53142e0b2d179b09da
Crois0509 <crois0509@naver.com>
2024-08-11 16:23:22 +0900

    feat: casting 함수 추가

--- CodeStarterCamp_Week5/main.swift
+++ CodeStarterCamp_Week5/main.swift
@@ -19,21 +19,4 @@ var manager: AuditionManager = AuditionManager(totalApplicantsList: [yagom,
 hacker.hackPassedApplicantsList()
 // 해커의 해킹
 
-for applicants in manager.totalApplicantsList {
-    do {
-        try manager.cast(to: applicants)
-    } catch TalentTypeError.notTalent {
-        print("불합격 사유: 탤런트가 아님")
-        print(MagicNumber.blank)
-    } catch TalentTypeError.badPersonality {
-        print("불합격 사유: 인성에 문제가 있어보임")
-        print(MagicNumber.blank)
-    } catch TalentTypeError.lackOfAbility {
-        print("불합격 사유: 능력치 부족")
-        print(MagicNumber.blank)
-    } catch {
-        print(error)
-    }
-}
-
-manager.announcePassedApplicants()
+casting(manager)
